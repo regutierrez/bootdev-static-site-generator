fe9403ccd5cf6301e668b16ec127ae94332e1ffd
regutierrez <11924623+regutierrez@users.noreply.github.com>
2024-03-22 15:59:29 -0400

    finish initial fixes for test

--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,14 @@
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 class TextNode:
     def __init__(self, text: str, text_type: str, url: str | None = None) -> None:
-        self.text: str  = text
+        self.text: str = text
         self.text_type: str = text_type
         self.url: str | None = url
 
