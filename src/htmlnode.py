ac6c01d49d895d39a8a00fb460bf916e1eae1eaa
regutierrez <11924623+regutierrez@users.noreply.github.com>
2024-03-22 16:34:13 -0400

    create new class

---
+++ src/htmlnode.py
@@ -0,0 +1,12 @@
+class HTMLNode:
+    def __init__(
+        self,
+        tag: str | None = None,
+        value: str | None = None,
+        children: list | None = None,
+        props: dict | None = None,
+    ) -> None:
+        self.tag: str | None = tag
+        self.value: str | None = value
+        self.children: list | None = children
+        self.props: dict | None = props
