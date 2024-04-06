34f91167d0574e2e135502b72e1bfddb658e9578
regutierrez <11924623+regutierrez@users.noreply.github.com>
2024-04-06 17:28:52 -0400

    fix bugs in markdown_to_blocks fcn

--- src/blocks.py
+++ src/blocks.py
@@ -1,17 +1,19 @@
 def markdown_to_blocks(text: str) -> list[str]:
-    blocks: list[str] = text.split("\n\n")
-
-    return blocks
+    return [block.strip() for block in text.split("\n\n") if block.strip() != ""]
 
 
 test = """
-This is **bolded** paragraph
+ This is **bolded** paragraph
 
-This is another paragraph with *italic* text and `code` here
+    
+ 
+
+ This is another paragraph with *italic* text and `code` here
 This is the same paragraph on a new line
 
 * This is a list
-* with items
+ * with items
+ 
 """
 
 print(markdown_to_blocks(test))
