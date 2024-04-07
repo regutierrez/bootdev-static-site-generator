62ed78d74fd86c4ce3dbfbb7fcc4a87761a0b9c1
regutierrez <11924623+regutierrez@users.noreply.github.com>
2024-04-06 21:15:44 -0400

    create block_to_block_type function;

--- src/blocks.py
+++ src/blocks.py
@@ -1,19 +1,29 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    BLOCK_TYPE_PARAGRAPH = "paragraph"
+    BLOCK_TYPE_HEADER = "header"
+    BLOCK_TYPE_QUOTE = "quote"
+    BLOCK_TYPE_CODE = "code"
+    BLOCK_TYPE_ULIST = "unordered_list"
+    BLOCK_TYPE_LIST = "ordered_list"
+
+
 def markdown_to_blocks(text: str) -> list[str]:
     return [block.strip() for block in text.split("\n\n") if block.strip() != ""]
 
 
-test = """
- This is **bolded** paragraph
-
-    
- 
-
- This is another paragraph with *italic* text and `code` here
-This is the same paragraph on a new line
-
-* This is a list
- * with items
- 
-"""
-
-print(markdown_to_blocks(test))
+def block_to_block_type(block: str) -> str:
+    if re.match(r"^(#{1,6})\s", block):
+        return BlockType.BLOCK_TYPE_HEADER.value
+    if re.match(r"^```[^`]+```$", block):
+        return BlockType.BLOCK_TYPE_CODE.value
+    if re.match(r"^>\s", block):
+        return BlockType.BLOCK_TYPE_QUOTE.value
+    if re.match(r"^[*-]\s", block):
+        return BlockType.BLOCK_TYPE_ULIST.value
+    if re.match(r"^\d+\.\s", block):
+        return BlockType.BLOCK_TYPE_LIST.value
+    return BlockType.BLOCK_TYPE_PARAGRAPH.value
