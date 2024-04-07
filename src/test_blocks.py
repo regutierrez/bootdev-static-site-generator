a035cd41037546911140e30f2aa4bdc01c75ef4b
regutierrez <11924623+regutierrez@users.noreply.github.com>
2024-04-06 21:29:27 -0400

    add tests for block_to_block_type;

--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestBlocks(unittest.TestCase):
@@ -26,6 +26,64 @@ This is the same paragraph on a new line
             "* This is a list\n * with items",
         ]
         self.assertEqual(markdown_to_blocks(test), expected)
+
+    def test_headers(self):
+        self.assertEqual(
+            block_to_block_type("### test"), BlockType.BLOCK_TYPE_HEADER.value
+        )
+        self.assertEqual(
+            block_to_block_type("# test"), BlockType.BLOCK_TYPE_HEADER.value
+        )
+        self.assertEqual(
+            block_to_block_type("###### test"), BlockType.BLOCK_TYPE_HEADER.value
+        )
+        self.assertEqual(
+            block_to_block_type("########### test"),
+            BlockType.BLOCK_TYPE_PARAGRAPH.value,
+        )
+
+    def test_code_block(self):
+        self.assertEqual(
+            block_to_block_type("```code```"), BlockType.BLOCK_TYPE_CODE.value
+        )
+        self.assertEqual(
+            block_to_block_type("```\nmulti-line code\n```"),
+            BlockType.BLOCK_TYPE_CODE.value,
+        )
+
+    def test_quote(self):
+        self.assertEqual(
+            block_to_block_type("> quote"), BlockType.BLOCK_TYPE_QUOTE.value
+        )
+        self.assertEqual(
+            block_to_block_type(">quote"), BlockType.BLOCK_TYPE_PARAGRAPH.value
+        )
+
+    def test_unordered_list(self):
+        self.assertEqual(
+            block_to_block_type("- item"), BlockType.BLOCK_TYPE_ULIST.value
+        )
+        self.assertEqual(
+            block_to_block_type("* item"), BlockType.BLOCK_TYPE_ULIST.value
+        )
+
+    def test_ordered_list(self):
+        self.assertEqual(
+            block_to_block_type("1. item"), BlockType.BLOCK_TYPE_LIST.value
+        )
+        self.assertEqual(
+            block_to_block_type("2 item"), BlockType.BLOCK_TYPE_PARAGRAPH.value
+        )
+        self.assertEqual(
+            block_to_block_type("10. item"), BlockType.BLOCK_TYPE_LIST.value
+        )
+
+    def test_paragraph(self):
+        self.assertEqual(
+            block_to_block_type("This is a paragraph."),
+            BlockType.BLOCK_TYPE_PARAGRAPH.value,
+        )
+        self.assertEqual(block_to_block_type(""), BlockType.BLOCK_TYPE_PARAGRAPH.value)
 
 
 if __name__ == "__main__":
