cd746ac0f0471fd09701ce42f65545fd5186c413
regutierrez <11924623+regutierrez@users.noreply.github.com>
2024-04-11 17:40:34 -0400

    code cleanup; refactored implemenation of generate page functions

--- src/generate_page.py
+++ src/generate_page.py
@@ -28,6 +28,10 @@ def generate_page(src: str, template_path: str, dst: str) -> None:
 
     converted_html: str = markdown_to_htmlnode(md).to_html()
 
+    dst_dir: str = os.path.dirname(dst)  # get path without the filename
+    if dst_dir != "":
+        os.makedirs(dst_dir, exist_ok=True)  # create dst directory if it does not exist
+
     with open(dst, "w") as file:
         file.write(
             template.replace("{{ Title }}", title).replace(
@@ -40,12 +44,21 @@ def generate_pages_recursively(src: str, template_path: str, dst: str) -> None:
     if not os.path.exists(src):
         raise Exception("source folder not found")
 
-    # list all .md file paths in the src directory. str(path) because path returns a Path object
-    src_subdir: list[str] = [str(path) for path in list(Path(src).glob("**/*.md"))]
+    for file in os.listdir(src):
+        file_src_path: str = os.path.join(src, file)
+        file_dst_path: str = os.path.join(dst, file)
+        if os.path.isfile(file_src_path):
+            new_dst_path: str = str(Path(file_dst_path).with_suffix(".html"))
+            generate_page(file_src_path, template_path, new_dst_path)
+        else:
+            generate_pages_recursively(file_src_path, template_path, file_dst_path)
 
-    for md in src_subdir:
-        new_dst_dir: str = os.path.join(dst, md.lstrip("content/").rstrip("index.md"))
-        if not os.path.isdir(new_dst_dir):
-            os.mkdir(new_dst_dir)
-        print(f"converting {md} to {new_dst_dir}")
-        generate_page(md, template_path, os.path.join(new_dst_dir, "index.html"))
+    # list all .md file paths in the src directory. str(path) because path returns a Path object
+    # src_subdir: list[str] = [str(path) for path in list(Path(src).glob("**/*.md"))]
+
+    # for md in src_subdir:
+    #     new_dst_dir: str = os.path.join(dst, md.lstrip("content/").rstrip("index.md"))
+    #     if not os.path.isdir(new_dst_dir):
+    #         os.mkdir(new_dst_dir)
+    #     print(f"converting {md} to {new_dst_dir}")
+    #     generate_page(md, template_path, os.path.join(new_dst_dir, "index.html"))
