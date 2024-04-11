96e9913d9f845f307860dad4bd589a8c80693ef5
regutierrez <11924623+regutierrez@users.noreply.github.com>
2024-04-11 15:18:05 -0400

    clean up and copystatic fcn bug fix

--- src/copystatic.py
+++ src/copystatic.py
@@ -10,7 +10,7 @@ def copy_static(src: str = "static", dst: str = "public") -> None:
     if not os.path.exists(src):
         raise Exception(f"{src} not found")
 
-    if not os.path.exists(dst) or os.path.isfile(dst):
+    if not os.path.exists(dst) and os.path.isdir(src):
         print(f"path {dst} does not exist. creating folder...")
         os.mkdir(dst)
     if os.path.isfile(src):
@@ -24,6 +24,3 @@ def copy_static(src: str = "static", dst: str = "public") -> None:
         item_dst_path: str = os.path.join(dst, item)
 
         copy_static(item_src_path, item_dst_path)
-
-
-copy_static()
